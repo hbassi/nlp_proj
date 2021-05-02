58c75527769b0f16d5278b9a3720dadf588806b1
Edmund Wang <wangedmund@berkeley.edu>
2021-05-02 04:02:58 -0700

    working on data agument file

--- data_augmentations.py
+++ data_augmentations.py
@@ -15,24 +15,28 @@ class DataAugmentation:
     def __init__(self, aug_num):
         self.PATH = None
         self.aug_num = aug_num
+        print(type(self.aug_num))
         self.data = pd.read_json('yelp_review_training_dataset.jsonl',lines=True)
-        if aug_num == 1:
+        if self.aug_num == '1':
             self.data = pd.read_json('/content/gdrive/MyDrive/nlp_proj/yelp_review_training_dataset.jsonl',lines=True)
             self.PATH = '/content/gdrive/MyDrive/nlp_proj/augmentations/'
-        elif aug_num == 2:
+        elif self.aug_num == '2':
+            print("HE")
             self.PATH = '/Users/edmundwang/Desktop/cs182/nlp_proj/augmentations/'
-        elif aug_num == 3:
+        elif self.aug_num == '3':
             self.PATH = '/Users/raymondwang/desktop/cs182/nlp_proj/augmentations/'
+        else:
+            print('The number entered does not correlate to a path')
 
     def run(self):
         X = self.data['text'][:5]
-        if self.aug_num == 1:
+        if self.aug_num == '1':
 
             aug = nac.KeyboardAug()
             augmented_text = X.apply(aug.augment)
             augmented_text.to_csv(self.PATH + 'testcolab.csv', index = False)
 
-        elif self.aug_num == 2:
+        elif self.aug_num == '2':
 
             aug_syn = naw.SynonymAug(aug_src='wordnet')
             augmented_text2 = X.apply(aug_syn.augment)
@@ -42,7 +46,7 @@ class DataAugmentation:
             augmented_text3 = X.apply(aug_rws.augment)
             augmented_text3.to_csv(self.PATH + 'testedmund2.csv', index=False)
 
-        elif self.aug_num == 3:
+        elif self.aug_num == '3':
 
             aug_del = naw.RandomWordAug(action='delete')
             augmented_text4 = X.apply(aug_del.augment)
@@ -50,5 +54,7 @@ class DataAugmentation:
 
 if __name__ == "__main__":
     args = sys.argv
-    print(args)
+    aug_num = args[1]
+    DataAugmentation(args[1]).run()
+
 
