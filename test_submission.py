e9a4d68feb904b2c1cb9850956cb05c743d3e128
Edmund Wang <wangedmund@berkeley.edu>
2021-05-07 18:10:12 -0700

    changed maxlength to be 150

--- test_submission.py
+++ test_submission.py
@@ -11,7 +11,7 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 '''
 Hyperparameters
 '''
-MAXLENGTH = 185
+MAXLENGTH = 150
 
 def eval(text):
 	# This is where you call your model to get the number of stars output
